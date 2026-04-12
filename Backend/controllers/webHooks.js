f81127183f92460f554de25c9a0c3dd5f2d82fd0
arifshaikh-official <arifshaikhofficial04@gmail.com>
2026-04-12 20:41:10 +0530

    some changes

--- Backend/controllers/webHooks.js
+++ Backend/controllers/webHooks.js
@@ -1,3 +1,57 @@
+// import { Webhook } from 'svix';
+// import User from '../models/User.js'
+
+// export const clerkWebhooks = async (req, res) => {
+//     try {
+//         const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
+//         await whook.verify(req.body.toString(), {
+//             "svix-id": req.headers["svix-id"],
+//             "svix-timestamp": req.headers["svix-timestamp"],
+//             "svix-signature": req.headers["svix-signature"]
+//         });
+         
+//         const { data, type } = JSON.parse(req.body.toString())
+
+//         switch (type) {
+//             case 'user.created': {
+//                 const userData = {
+//                     _id: data.id,
+//                     email: data.email_addresses[0].email_address,
+//                     name: data.first_name + " " + data.last_name,
+//                     image: data.image_url,  
+//                     resume: ' '
+//                 }
+//                 await User.create(userData)
+//                 res.json({})
+//                 break;
+//             }
+//             case 'user.updated': {      
+//                  const userData = {
+//                     email: data.email_addresses[0].email_address,
+//                     name: data.first_name + " " + data.last_name,
+//                     image: data.image_url
+//                  }  
+//                  await User.findByIdAndUpdate(data.id, userData) 
+//                 res.json({}) 
+//                 break;
+//             }
+//             case 'user.deleted': {
+//                await User.findByIdAndDelete(data.id)
+//                res.json({})
+//                 break;
+//             }
+//             default:
+//                 break;
+//         }
+//     } catch (error) {
+//         console.log(error.message);
+//         res.json({ success: false, message: 'Webhooks Error' });
+//     }
+// };
+
+
+
+
 import { Webhook } from 'svix';
 import User from '../models/User.js'
 
@@ -9,42 +63,39 @@ export const clerkWebhooks = async (req, res) => {
             "svix-timestamp": req.headers["svix-timestamp"],
             "svix-signature": req.headers["svix-signature"]
         });
-         
+
         const { data, type } = JSON.parse(req.body.toString())
 
         switch (type) {
             case 'user.created': {
                 const userData = {
                     _id: data.id,
-                    email: data.email_addresses[0].email_address,
+                    email: data.email_addresses[0]?.email_address || '',
                     name: data.first_name + " " + data.last_name,
-                    image: data.image_url,  
-                    resume: ' '
+                    image: data.image_url,
+                    resume: ''
                 }
                 await User.create(userData)
-                res.json({})
-                break;
+                return res.json({ success: true })
             }
-            case 'user.updated': {      
-                 const userData = {
-                    email: data.email_addresses[0].email_address,
+            case 'user.updated': {
+                const userData = {
+                    email: data.email_addresses[0]?.email_address || '',
                     name: data.first_name + " " + data.last_name,
                     image: data.image_url
-                 }  
-                 await User.findByIdAndUpdate(data.id, userData) 
-                res.json({}) 
-                break;
+                }
+                await User.findByIdAndUpdate(data.id, userData)
+                return res.json({ success: true })
             }
             case 'user.deleted': {
-               await User.findByIdAndDelete(data.id)
-               res.json({})
-                break;
+                await User.findByIdAndDelete(data.id)
+                return res.json({ success: true })
             }
             default:
-                break;
+                return res.json({ success: false, message: 'Unknown event' })
         }
     } catch (error) {
-        console.log(error.message);
-        res.json({ success: false, message: 'Webhooks Error' });
+        console.log("WEBHOOK ERROR:", error.message)
+        res.status(400).json({ success: false, message: error.message })
     }
 };
