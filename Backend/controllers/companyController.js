bdc66e4c398039e7122c24a47976acd862a1d8ea
arifshaikh-official <arifshaikhofficial04@gmail.com>
2026-04-13 19:41:34 +0530

    some changes

--- Backend/controllers/companyController.js
+++ Backend/controllers/companyController.js
@@ -4,6 +4,7 @@ import { v2 as cloudinary } from 'cloudinary'
 import generateToken from "../utils/generateToken.js";
 import Job from "../models/Job.js";
 import JobApplications from '../models/JobApplications.js'
+import JobApplication from "../models/JobApplications.js";
 
 // Company register
 export const registerCompany = async (req, res) => {
@@ -76,15 +77,15 @@ export const loginCompany = async (req, res) => {
 export const getCompanyData = async (req, res) => {
     try {
         const company = req.company
-        res.json({success: true, company})
+        res.json({ success: true, company })
     } catch (error) {
-        res.json({success: false, message: error.message})
+        res.json({ success: false, message: error.message })
     }
 }
 
 // Post a new job
 export const postJob = async (req, res) => {
-    const {title, description, location, salary, level, category} = req.body
+    const { title, description, location, salary, level, category } = req.body
     const companyId = req.company._id
     try {
         const newJob = await Job({
@@ -98,22 +99,22 @@ export const postJob = async (req, res) => {
             category
         })
         await newJob.save()
-        res.json({success: true, newJob})
+        res.json({ success: true, newJob })
     } catch (error) {
-        res.json({success: false, message: error.message})
+        res.json({ success: false, message: error.message })
     }
 }
 
 export const getCompanyJobApplicants = async (req, res) => {
     try {
         const companyId = req.company._id
-        const applications = await JobApplications.find({companyId})
-        .populate('userId','name image resume')
-        .populate('jobId','title location category level salary')
-        .exec()
-        return res.json({success: true, applications})
+        const applications = await JobApplications.find({ companyId })
+            .populate('userId', 'name image resume')
+            .populate('jobId', 'title location category level salary')
+            .exec()
+        return res.json({ success: true, applications })
     } catch (error) {
-        res.json({success: false, message: error.message})
+        res.json({ success: false, message: error.message })
     }
 }
 
@@ -121,39 +122,46 @@ export const getCompanyJobApplicants = async (req, res) => {
 export const getCompanyPostedJobs = async (req, res) => {
     try {
         const companyId = req.company._id
-        const jobs = await Job.find({companyId})
+        const jobs = await Job.find({ companyId })
 
         // Adding no. of applicants info in data
         const jobsData = await Promise.all(jobs.map(async (job) => {
-            const applicants = await JobApplications.find({jobId: job._id});  
-            return {...job.toObject(),applicants:applicants.length}
+            const applicants = await JobApplications.find({ jobId: job._id });
+            return { ...job.toObject(), applicants: applicants.length }
         }))
 
-        res.json({success: true, jobsData: jobs})
+        res.json({ success: true, jobsData: jobs })
     } catch (error) {
-        res.json({success: false, message: error.message})
+        res.json({ success: false, message: error.message })
     }
 }
 
 // Change job application status
 export const changeJobApplicationsStatus = async (req, res) => {
+    try {
+        const { id, status } = req.body
+        await JobApplication.findOneAndUpdate({ _id: id }, { status })
+        res.json({ success: true, message: 'Status Changed' })
 
+    } catch (error) {
+        res.json({ success: false, message: error.message })
+    }
 }
 
 // Change job visibilty
 export const changeVisibility = async (req, res) => {
     try {
-        const {id} = req.body
+        const { id } = req.body
         const companyId = req.company._id
         const job = await Job.findById(id)
-        if(companyId.toString() === job.companyId.toString()) {
+        if (companyId.toString() === job.companyId.toString()) {
             job.visible = !job.visible
         }
         await job.save()
-        res.json({success: true, job})
-        
+        res.json({ success: true, job })
+
     } catch (error) {
-        res.json({success: false, message: error.message})
+        res.json({ success: false, message: error.message })
     }
 }
 
--- Frontend/src/pages/Applications.jsx
+++ Frontend/src/pages/Applications.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../components/Navbar'
 import { assets, jobsApplied } from '../assets/assets'
 import moment from 'moment'
@@ -16,7 +16,7 @@ const Applications = () => {
 
   const [isEdit, setIsEdit] = useState(false)
   const [resume, setResume] = useState(null)
-  const {backendUrl, userData, userApplications, fetchUserData} = useContext(AppContext)
+  const {backendUrl, userData, userApplications, fetchUserData, fetchUserApplications} = useContext(AppContext)
 
   const updateResume = async () => {
     try {
@@ -40,6 +40,12 @@ const Applications = () => {
     setIsEdit(false)
     setResume(null)
   }
+
+  useEffect(()=>{
+    if (user) {
+      fetchUserApplications()
+    }
+  },[user])
 
   return (
     <>
