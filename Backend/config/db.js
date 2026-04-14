79d0ba593039b63f1fb754d94ec456ca237193ac
arifshaikh-official <arifshaikhofficial04@gmail.com>
2026-04-14 14:51:20 +0530

    some changes

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
 const connectDb = async () => {
     mongoose.connection.on('connected',() => console.log("Database Connected"))
-    await mongoose.connect(`${process.env.MONGODB_URI}/job-portal`)
+    await mongoose.connect(process.env.MONGODB_URI)
 }
 
 export default connectDb;
